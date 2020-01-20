d862e8274686eb1dcc83812d92d9ffd1e1101138
Raitichan <rusuke@t-net.ne.jp>
2020-01-20 13:56:14 +0900

    a

--- AudioAnalyzer/AudioFile.cs
+++ AudioAnalyzer/AudioFile.cs
@@ -1,6 +1,8 @@
-﻿using NAudio.Wave;
+﻿using NAudio.Dsp;
+using NAudio.Wave;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -46,13 +48,72 @@ namespace AudioAnalyzer {
 		/// <param name="fileName">音声ファイル名</param>
 		public AudioFile(string fileName) {
 			this.FileName = fileName;
-			this.audioStream = new AudioFileReader(fileName);
+			this.audioStream = new AudioFileReader(fileName) {
+				Position = 0
+			};
 		}
 
 		public Task ExportTextureAsync(IProgress<int> progress) => Task.Run(() => {
-			progress.Report(50);
-			Thread.Sleep(5000);
-			progress.Report(100);
+			progress.Report(0);
+
+			int blockLength = 256; // サンプルブロックのデータ長
+			float[] audioData = new float[this.audioStream.Length / this.audioStream.BlockAlign * this.audioStream.WaveFormat.Channels];
+			Complex[] buffer = new Complex[blockLength]; // サンプル用バッファー
+
+			this.audioStream.Read(audioData, 0, audioData.Length);
+			AudioTexture texture = new AudioTexture();
+
+			/*
+			int fftPos = 0;
+			int putPix = 0;
+			for (int i = 0; i < audioData.Length; i++) {
+				buffer[fftPos].X = (float)(audioData[i] * FastFourierTransform.HammingWindow(fftPos, blockLength));
+				buffer[fftPos].Y = 0.0f;
+				fftPos++;
+				if (blockLength <= fftPos) {
+					fftPos = 0;
+					int m = (int)Math.Log(blockLength, 2.0);
+					FastFourierTransform.FFT(true, m, buffer);
+
+					for (int j = 0; j < blockLength; j++) {
+						double diagonal = Math.Sqrt(buffer[j].X * buffer[j].X + buffer[j].Y * buffer[j].Y);
+						texture.Put(putPix, j, diagonal * 100);
+					}
+					putPix++;
+					progress.Report((int)((double)i / audioData.Length * 100));
+				}
+				
+			}
+			*/
+
+			int pixcel = audioData.Length / 8192;
+			int pixcelIndex = 0;
+			for (int i = 0; i < audioData.Length; i+= pixcel) {
+				int audioDataStartIndex = i - (blockLength / 2); // サンプルブロックの先頭インデックス
+				for (int j = 0; j < buffer.Length; j++) {
+					int audioDataIndex = audioDataStartIndex + j; // サンプルデータのインデックス
+					buffer[j].X = (float)(audioDataIndex < 0 ? 0 : audioDataIndex >= audioData.Length ? 0 : (audioData[audioDataIndex] * FastFourierTransform.HammingWindow(j, blockLength)));
+					buffer[j].Y = 0;
+				}
+
+				int m = (int)Math.Log(blockLength, 2.0);
+				FastFourierTransform.FFT(true, m, buffer);
+
+				for (int j = 0; j < buffer.Length / 2; j++) {
+					double diagonal = Math.Sqrt(buffer[j].X * buffer[j].X + buffer[j].Y * buffer[j].Y);
+					double intensityDB = 10.0 * Math.Log(diagonal);
+					double percent = 1.0 - ((intensityDB < -60.0) ? 1.0 : intensityDB / -60.0);
+					texture.Put(pixcelIndex, j, percent);
+
+				}
+				pixcelIndex++;
+				progress.Report((int)((double)i / audioData.Length * 100));
+			}
+			
+
+			texture.SaveTexture(@"./test.png");
+			progress.Report(0);
+
 		});
 	}
 }
---
+++ AudioAnalyzer/AudioTexture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioAnalyzer {
+	/// <summary>
+	/// 音声テクスチャオブジェクト
+	/// </summary>
+	internal class AudioTexture {
+
+		/// <summary>
+		/// 画像の幅(px)
+		/// </summary>
+		public static readonly int Width = 8192;
+		/// <summary>
+		/// 画像の高さ(px)
+		/// </summary>
+		public static readonly int Hight = 128;
+
+		#region Private Field
+		/// <summary>
+		/// 画像データ
+		/// </summary>
+		private Bitmap bitmap;
+
+		#endregion
+
+		/// <summary>
+		/// 画像テクスチャの作成
+		/// </summary>
+		public AudioTexture() => this.bitmap = new Bitmap(Width, Hight);
+
+		public void Put(int x, int y, double level) {
+			if (x >= Width || y >= Hight) {
+				return;
+			}
+
+			this.bitmap.SetPixel(x, y, Color.FromArgb(Math.Min((int)(level * 255), 255), 0, 0));
+		}
+
+		public void SaveTexture(string fileName) => this.bitmap.Save(fileName, ImageFormat.Png);
+	}
+}
